90dab0968a33b0781de6b9b69e3fa3f864c2d9b9
szymon <xxxxxxszymi@gmail.com>
2025-06-05 21:39:43 +0200

    Little changes

--- global/global.js
+++ global/global.js
@@ -1,2 +1,12 @@
 includeHTML()
 
+function toggleBurger() {
+    let burgerIcon = document.getElementById('burger-menu');
+    let burgerMenu = document.getElementById('menu');
+    burgerMenu.classList.toggle('toggled');
+    if (burgerMenu.classList.contains('toggled')) {
+        burgerIcon.setAttribute('src', '../Assets/images/xmark-solid.svg');
+    } else{
+        burgerIcon.setAttribute('src', '../Assets/images/bars-solid.svg');
+    }
+}
